e7799a92352de64fdad8fedebe5c2535743bfdf9
Kartavya Chauhan <kartavyachauhan2005@gmail.com>
2025-05-29 15:19:43 +0530

    Confirm CORS is enabled

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins (update to specific origins in production)
+  });
   await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
