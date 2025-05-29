dedb0dd00e72187c676ee8435d8cb0b624ae1e58
Kartavya Chauhan <kartavyachauhan2005@gmail.com>
2025-05-29 17:26:30 +0530

    Debugging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*', // Allow all origins (update to specific origins in production)
+app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allow these methods
+    allowedHeaders: 'Content-Type,Authorization', // Allow these headers
   });
   await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
+
+
